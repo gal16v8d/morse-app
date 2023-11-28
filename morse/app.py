c2580dca550c728fa6626ba74e7ec74c22d60b91
alexg <alex.galvis.sistemas@gmail.com>
2023-11-27 20:30:52 -0500

    Update codebase

--- morse/app.py
+++ morse/app.py
@@ -1,17 +1,47 @@
+'''Allows to get morse value of any valid input'''
 import re
-from utils.letters import Letters
+import string
+from typing import Any
 
 
-def show_morse(input):
-    letters = Letters()
+ALPHABET = list(string.ascii_lowercase)
+MORSE = ['·-', '-···', '-·-·', '-··', '·',
+         '··-·', '--·', '····', '··', '·---', '-·-',
+         '·-··', '--', '-·', '---', '·--·', '--·-',
+         '·-·', '···', '-', '··-', '···-', '·--',
+         '-··-', '-·--', '--··']
+MORSE_INT = ['-----', '·----', '··---', '···--', '····-',
+             '·····', '-····', '--···', '---··', '----·']
+
+
+def is_integer(value: Any):
+    '''Return true if value is int, false otherwise'''
+    return isinstance(value, int)
+
+
+def pos_in_alphabet(value: string):
+    '''Return index of value inside ALPHABET arr, -1 otherwise'''
+    return ALPHABET.index(value) if value in ALPHABET else -1
+
+
+def convert_to_morse(value: Any):
+    '''Return morse result of single value input'''
+    if is_integer(value):
+        return MORSE_INT[int(value)]
+    index = pos_in_alphabet(value)
+    return MORSE[index]if index != -1 else ''
+
+
+def show_morse(value: Any):
+    '''Return morse result of full value input'''
     result = ''
-    for n in input:
-        result += letters.convert_to_morse(n)
-    print('{0} en morse es: {1}'.format(input, result))
+    for n in value:
+        result += convert_to_morse(n)
+    print(f'{value} en morse es: {result}')
 
 
 if __name__ == '__main__':
-    user_input = input("Ingresa texto:")
+    user_input = input('Ingresa texto:')
     if re.match('^[a-zA-Z0-9]+$', user_input):
         name_array = user_input.lower().split()
         show_morse(user_input)
